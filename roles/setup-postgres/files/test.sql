a1171cb9d9bfbb1b719983f7aa9cca6d0a0a538b
Andre Mercer <amercer1@email.arizona.edu>
2015-10-26 16:05:06 -0700

    Add run sql file command for atmosphere database 	new file:   setup-postgres/files/test.sql 	modified:   setup-postgres/tasks/main.yml 	modified:   setup-postgres/vars/main.yml

---
+++ roles/setup-postgres/files/test.sql
@@ -0,0 +1,9 @@
+drop table andre_table_test;
+
+create table andre_table_test(
+        id integer,
+        first_name text,
+        last_name text
+);
+
+insert into andre_table_test(id, first_name, last_name) values (0, 'Andre', 'Mercer');
