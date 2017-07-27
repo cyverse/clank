6f75b215b7944e4b2c99dc7cb5d71b0c65671273
Lucas H. Xu <xuh@bu.edu>
2017-07-27 11:46:51 -0400

    get rid of version

--- tests/setup_centos_tests.sh
+++ tests/setup_centos_tests.sh
@@ -1,14 +1,12 @@
 #!/bin/sh -xe
 
 
-OS_VERSION=$1
-
 # This script starts docker and systemd (if el7)
 
-docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/clank:rw  centos:centos${OS_VERSION}   /usr/sbin/init
+docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/clank:rw  centos:centos${7}   /usr/sbin/init
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 docker logs $DOCKER_CONTAINER_ID
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /clank/tests/test_inside_docker.sh ${OS_VERSION};
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /clank/tests/test_inside_docker.sh ${7};
   echo -ne \"------\nEND CLANK TESTS\n\";"
 docker ps -a
 docker stop $DOCKER_CONTAINER_ID
