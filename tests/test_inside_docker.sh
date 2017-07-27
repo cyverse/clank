8e884a6a4657254daf2817a05c52aaa641261966
Lucas H. Xu <xuh@bu.edu>
2017-07-27 13:51:56 -0400

    setup-postgres and fix line

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -10,11 +10,4 @@ virtualenv clank_env
 source clank_env/bin/activate
 pip install -r requirements.txt
 cp dist_files/variables.yml.dist variables.yml
-clank_env/bin/python ./clank.py
---env_file variables.yml
--x "atmosphere_github_branch=\"${ATMO_GITHUB_BRANCH:-master}\""
--x "atmosphere_github_repo=\"${ATMO_GITHUB_REPO:-https://github.com/CyVerse/atmosphere.git}\""
--x "troposphere_github_branch=\"${TROPO_GITHUB_BRANCH:-master}\""
--x "troposphere_github_repo=\"${TROPO_GITHUB_REPO:-https://github.com/CyVerse/troposphere.git}\""
---skip-tags "distro-update,create-nginx-uwsgi,enable-services"
--b
+clank_env/bin/python ./clank.py --env_file variables.yml -x "atmosphere_github_branch=\"${ATMO_GITHUB_BRANCH:-master}\"" -x "atmosphere_github_repo=\"${ATMO_GITHUB_REPO:-https://github.com/CyVerse/atmosphere.git}\"" -x "troposphere_github_branch=\"${TROPO_GITHUB_BRANCH:-master}\"" -x "troposphere_github_repo=\"${TROPO_GITHUB_REPO:-https://github.com/CyVerse/troposphere.git}\"" --skip-tags "distro-update,create-nginx-uwsgi,enable-services" -b
