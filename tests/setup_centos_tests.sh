f1b921fa5f52b66ec29a517bc178faa43692aac4
Lucas H. Xu <xuh@bu.edu>
2017-07-27 11:07:19 -0400

    fix cannot find command

--- tests/setup_centos_tests.sh
+++ tests/setup_centos_tests.sh
@@ -8,7 +8,7 @@ OS_VERSION=$1
 docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/clank:rw  centos:centos${OS_VERSION}   /usr/sbin/init
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 docker logs $DOCKER_CONTAINER_ID
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /clank/test_inside_docker.sh ${OS_VERSION};
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /clank/tests/test_inside_docker.sh ${OS_VERSION};
   echo -ne \"------\nEND CLANK TESTS\n\";"
 docker ps -a
 docker stop $DOCKER_CONTAINER_ID
