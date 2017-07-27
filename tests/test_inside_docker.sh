d268707e2c153d23f88779fb8cda558b88eeb27d
Lucas H. Xu <xuh@bu.edu>
2017-07-27 12:55:57 -0400

    add tests

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -4,8 +4,16 @@
 ls -l /home
 
 yum install -y git python python-pip python-devel libffi-devel openssl-devel
-yum groupinstall "Development tools"
+yum groupinstall -y "Development tools"
 virtualenv clank_env
 source clank_env/bin/activate
 pip install -r requirements.txt
 cp dist_files/variables.yml.dist variables.yml
+clank_env/bin/python ./clank.py
+--env_file variables.yml
+-x "atmosphere_github_branch=\"${ATMO_GITHUB_BRANCH:-master}\""
+-x "atmosphere_github_repo=\"${ATMO_GITHUB_REPO:-https://github.com/CyVerse/atmosphere.git}\""
+-x "troposphere_github_branch=\"${TROPO_GITHUB_BRANCH:-master}\""
+-x "troposphere_github_repo=\"${TROPO_GITHUB_REPO:-https://github.com/CyVerse/troposphere.git}\""
+--skip-tags "distro-update,create-nginx-uwsgi,enable-services"
+-b
