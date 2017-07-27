5703cac069cce023994e37cbeff2c65f032068e4
Lucas H. Xu <xuh@bu.edu>
2017-07-27 12:02:18 -0400

    fix typo

--- tests/setup_centos_tests.sh
+++ tests/setup_centos_tests.sh
@@ -3,7 +3,7 @@
 
 # This script starts docker and systemd (if el7)
 
-docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/rclank:rw  centos:centos7   /usr/sbin/init
+docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/clank:rw  centos:centos7   /usr/sbin/init
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 docker logs $DOCKER_CONTAINER_ID
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /clank/tests/test_inside_docker.sh;
